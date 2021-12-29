be7a75fd8fdd13665c44d80a7177bf5c9e5bda17
LeoEmVee <santito79@hotmail.com>
2021-12-29 19:24:12 +0100

    Add component Loading. Update parent ProductLayout accordingly. UI structure complete

---
+++ client/src/Products/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'; // Importa framework React.
+import { Loader } from 'react-bulma-components'; // Importa componente Loader react-bulma-components.
+
+const Loading = () => { // Función que retorna el componente que luego será renderizado.
+  return ( // Contenido del componente que se retorna.
+    <div className="columns is-centered">
+      <Loader style={{width: 200, heigth: 200}}>TEXTOPARAQUESEMEVEALOADING</Loader>
+    </div>
+    
+  )
+}
+
+export default Loading; // Exporta Loading, que será llamado en ProductLayout, componente padre de este.
--- client/src/Products/components/ProductLayout.js
+++ client/src/Products/components/ProductLayout.js
@@ -1,12 +1,14 @@
 import React from 'react'; // Importa framework React.
 import Header from './Header'; // Importa Header, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
 import AddButton from './AddButton'; // Importa AddButton, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
+import Loading from './Loading'; // Importa Loading, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
 
-const ProductLayout = () => { // Función que retorna el componente que luego será renderizado. De momento, un string de ejemplo, que se verá en el browser).
+const ProductLayout = () => { // Función que retorna el componente que luego será renderizado: los componentes hijos importados Header, AddButton y Loading.
   return (
     <>
       <Header title="Seguimiento de productos"></Header>
       <AddButton></AddButton>
+      <Loading></Loading>
     </>
   );
 }
