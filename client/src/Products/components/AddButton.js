e06a5f1163afea8575b097098455286b6ef73c29
LeoEmVee <santito79@hotmail.com>
2021-12-30 19:07:58 +0100

    Add form for product adding to ProductLayout (Bulma Modal). Add functionality to AddButton (now toggles form)

--- client/src/Products/components/AddButton.js
+++ client/src/Products/components/AddButton.js
@@ -1,12 +1,12 @@
 import React from 'react'; // Importa framework React.
 import { Container, Section, Button } from 'react-bulma-components'; // Importa componentes Container, Section y Button de react-bulma-components.
 
-const AddButton = () => { // Función que retorna el componente que luego será renderizado.
+const AddButton = ({ onClick }) => { // Función que retorna el componente que luego será renderizado. Recibe como argumento onCLick (que se ejecutará cuando se pulse el botón).
   return ( // Contenido del componente que se retorna.
     <Section>
       <Container>
         <div className="is-pulled-right">
-          <Button color="primary">Añadir producto</Button>
+          <Button onClick={onClick} color="primary">Añadir producto</Button>
         </div>
       </Container>
     </Section>
--- client/src/Products/components/ProductLayout.js
+++ client/src/Products/components/ProductLayout.js
@@ -1,14 +1,27 @@
-import React from 'react'; // Importa framework React y hook useState (un hook es una función especial de React para manejar estados del componente).
+import React, { useState } from 'react'; // Importa framework React y hook useState (un hook es una función especial de React para manejar estados del componente).
+import { Modal } from 'react-bulma-components'; // Importa Modal de react-bulma-components. Un modelo de formulario.
 import Header from './Header'; // Importa Header, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
 import AddButton from './AddButton'; // Importa AddButton, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
 import ListProducts from './ListProducts'; // Importa ListProducts, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
 
-const ProductLayout = () => { // Función que retorna el componente que luego será renderizado: los componentes hijos importados Header, AddButton y ListProducts (con el estado que le corresponda).
+const ProductLayout = () => { // Función que retorna el componente que luego será renderizado: los componentes hijos importados Header, AddButton y ListProducts (con el estado que le corresponda), y el componente Modal de Bulma, para crear el formulario de entrada de productos. Añadido método onClick al AddButton, con el que se abre el formulario (setIsModalOpen(true)).
+  const [isModalOpen, setIsModalOpen] = useState(false); // useState retorna dos valores: una variable de estado (en este caso true o false, el argumento que recibe useState) y una función de actualización del estado.
+  
   return (
     <>
       <Header title="Seguimiento de productos"></Header>
-      <AddButton></AddButton>
+      <AddButton onClick={() => setIsModalOpen(true)}></AddButton>
       <ListProducts></ListProducts>
+      <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
+      <Modal.Card>
+        <Modal.Card.Header>
+          Formulario
+        </Modal.Card.Header>
+        <Modal.Card.Body>
+          Formulario aquí
+        </Modal.Card.Body>
+      </Modal.Card>
+      </Modal>
     </>
   );
 }
