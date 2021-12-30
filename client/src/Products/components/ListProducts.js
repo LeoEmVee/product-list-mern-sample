f278e04833ca1a3c583d6c8aad530ea2fe58a821
LeoEmVee <santito79@hotmail.com>
2021-12-30 17:23:57 +0100

    Add method getProducts. Update ListProducts accordingly. Install Axios and Cors. Delete unnecessary code

--- client/src/Products/components/ListProducts.js
+++ client/src/Products/components/ListProducts.js
@@ -1,20 +1,38 @@
 import React, { useState, useEffect } from 'react'; // Importa framework React y hooks useState (un hook es una función especial de React para manejar estados del componente) y useEffect (para manejar el ciclo de vida del componente React, ejecutando efectos secundarios).
+import axios from 'axios'; // Importa librería Axios, que sirve para hacer peticiones a endpoints o a URLs.
 import Loading from './Loading';  // Importa Loading, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
+
+const baseUrl = 'http://localhost:8081/v1'; // Referencia al archivo del backend. Aquí es donde está la DB cuyos datos se rescatan.
+
+async function getProducts () { // Función para recuperar los productos de la DB.
+  try { // Intenta:
+    const response = await axios({ // Recibe como parámetro un objeto con las propiedades:
+      url: `${baseUrl}/products`, // Su valor es la baseUrl y el endpoint donde están los productos.
+      method: 'GET' // El método de la request. GET es el default, pero lo especificamos para que se vea.
+    })
+    return response;
+  } catch (error) { // Devuelve error en caso de no lograr recuperar los datos.
+    console.log(error);
+  }
+
+}
 
 const ListProducts = () => { // Función que retorna el componente que luego será renderizado.
   const [isLoading, setIsLoading] = useState(true); // useState retorna dos valores: una variable de estado (en este caso true o false, el argumento que recibe useState) y una función de actualización del estado.
   // Equivale a: const state = useState(true), state[0] = isLoading, state[1] = setIsLoading.
-  useEffect(() => { // Función que se ejecuta después del renderizado. Equivale a componentDidMount(), componentDidUpdate() y componentWillMount() (que necesitan render() antes del return) en componentes tipo clase. Sirve para actualizar estados cuando hay algún cambio en backend, o para actualizaciones en una suscripción (en ese caso el efecto debe ser ejecutado con saneamiento para evitar saturar la memoria).
-    const timeID = setInterval(() => { // Define un untervalo de tiempo para el el cambio de estado del componente.
-      setIsLoading(!isLoading); // Cambia el estado de isLoading a false para mostrar el listado de productos una vez renderizado.
-    }, 3000)
-    
-    return () => clearInterval(timeID); // Saneamiento. Retorna clearInterval con el timeID definido. Pide quitar el setInterval para evitar saturación en la memoria (por generar el intervalo cada vez).
-  });
+  const [products, setProducts] = useState([]); // useState retorna los productos de la base de datos. Una es el estado de los productos (si existen o no), y la otra actualiza el estado (según haya productos introducidos o no).
 
-  useEffect(() => { // useEffect de ejemplo para evitar ejecuciones innecesarias. Sin saneamiento.
-    console.log('Only one time');
-  }, []); // El [] evita que el useEffect se ejecute a cada nuevo renderizado ocasionado por algún cambio. React lo toma como argumento cuyos cambios ha de vigilar, y como no cambia, evita que el console.log se ejecute de nuevo. Si en el [] estuviera, por ejemplo, isLoading, se ejecutaría cada vez, porque el parámetro cambia por efecto del useEffect anterior.
+  useEffect(() => { // Función que se ejecuta después del renderizado. Equivale a componentDidMount(), componentDidUpdate() y componentWillMount() (que necesitan render() antes del return) en componentes tipo clase. Sirve para actualizar estados cuando hay algún cambio en backend, o para actualizaciones en una suscripción (en ese caso el efecto debe ser ejecutado con saneamiento para evitar saturar la memoria).
+    (async function () { // Declara que la respuesta (lo que será el componente) de la función será el resultado de getProducts() cada vez que se actualice el estado del componente.
+      const response = await getProducts();
+
+      if (response.status === 200) {
+        setProducts(response.data.products);
+        console.log(response);
+      }
+    })();
+  }, []); // El [] como segundo argumento de useEffect evita que la función asíncrona se ejecute si no hay cambios en la base de datos (porque el array vacío no cambia).
+
   
   return ( // Contenido del componente que se retorna.
     isLoading ? <Loading></Loading> : 'Mostrar listado de productos'
--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express'); // Importa framework express.
-const app = express(); // Crea aplicación express.
+const cors = require('cors'); // Importa el paquete Cors para habilitar el cross origin resource sharing.
 const productRoutes = require('./routes/product.route'); // Importa las rutas del archivo product.route.
 
+const app = express(); // Crea aplicación express.
+
+app.use(cors()); // Dice a la app de usar cors, para evitar bloqueo de tráfico de datos de distintos orígenes.
 app.use(express.json()); // Dice a la app de usar el método json() para parsear datos que vengan posteados en ese formato.
 app.use(express.urlencoded({extended: false})); // Dice a la app de usar el método urlencoded() para parsear datos que vengan posteados en ese formato.
 app.use('/public', express.static(`${__dirname}/storage/images`)); // Dice a la app que a la ruta que está en /public le sirva (estático) lo que haya en la carpeta de images. Fuera de la API no se conoce esta ubicación (el usuario ve /public).
