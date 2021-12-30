f8e99a59ab34073d50be0339101d5cfc19c23536
LeoEmVee <santito79@hotmail.com>
2021-12-30 11:02:24 +0100

    Add hook useState (ProductLayout) for switching state of Loading and components-design file

--- client/src/Products/components/ProductLayout.js
+++ client/src/Products/components/ProductLayout.js
@@ -1,14 +1,20 @@
-import React from 'react'; // Importa framework React.
+import React, { useState } from 'react'; // Importa framework React y hook useState (un hook es una función especial de React para manejar estados del componente).
 import Header from './Header'; // Importa Header, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
 import AddButton from './AddButton'; // Importa AddButton, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
 import Loading from './Loading'; // Importa Loading, componente hijo, para retornarlo en la función de este componente (lo que luego será renderizado).
+import { Button } from 'react-bulma-components'; // Importa botón de Bulma.
 
-const ProductLayout = () => { // Función que retorna el componente que luego será renderizado: los componentes hijos importados Header, AddButton y Loading.
+const ProductLayout = () => { // Función que retorna el componente que luego será renderizado: los componentes hijos importados Header, AddButton y Loading (con el estado que le corresponda). El Button está de ejemplo para cambiar el estado de Loading (isLoading).
+  const [isLoading, setIsLoading] = useState(true); // useState retorna dos valores: una variable de estado (en este caso true o false, el argumento que recibe useState) y una función de actualización del estado.
+  // Equivale a: const state = useState(true), state[0] = isLoading, state[1] = setIsLoading.
   return (
     <>
       <Header title="Seguimiento de productos"></Header>
       <AddButton></AddButton>
-      <Loading></Loading>
+      <Button onClick={() => setIsLoading(!isLoading)}>Update state</Button>
+      {
+        isLoading ? <Loading></Loading> : 'Mostrar resultado fetch'
+      }
     </>
   );
 }
